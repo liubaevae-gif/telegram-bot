5d884bbca0aebaad4139f34e6d633498510a5344
liubaevae-gif <liubaevae@gmail.com>
2025-10-07 11:36:53 +0300

    Update and rename bot.py to app.py

--- app.py
+++ app.py
@@ -262,4 +262,5 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     print("Бот запущен...")
+
     app.run_polling()
